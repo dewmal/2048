231275959da429dabfbc7d389ff4ed494d3027fa
dewmal <dewmalnilanka@gmail.com>
2014-03-13 10:11:04 +0530

    init happy devil

--- js/application.js
+++ js/application.js
@@ -1,4 +1,18 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+    new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+});
+
+$(document).ready(function () {
+
+    $('.show-link').click(function () {
+        $('.game-help').show();
+        $(this).hide();
+    });
+    $('.close-link').click(function () {
+        $('.game-help').hide(function () {
+            $('.show-link').show();
+        });
+
+    });
 });
